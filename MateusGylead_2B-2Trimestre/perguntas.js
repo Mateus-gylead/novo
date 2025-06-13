2217b2e6c339d399562195c2e6411a9810b47e0a
Mateus-gylead <alves.goncalves.mateus@escola.pr.gov.br>
2025-06-13 09:28:14 -0300

    Add files via upload

---
+++ MateusGylead_2B-2Trimestre/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'historia',
+    'Quando aconteceu a segunda guerra mundial?',
+    'Aconteceu no dia 1 de setembro de 1939..'
+)
+
+criaCartao(
+    'Educação fisica',
+    'oque é RM?',
+    'É a repetição maxima que conseguimos fazer com determinado peso'
+)
+
+criaCartao(
+    'Esporte',
+    'Quem foi o maior bodybuilder?',
+    'Ronnie Coleman'
+)
+
+criaCartao(
+    'Robotica',
+    'Oque é uma protoboard?',
+    'É uma placa de plástico com furos condutores que facilitam a montagem temporária de circuitos eletrónicos, sem a necessidade de soldagem'
+)
